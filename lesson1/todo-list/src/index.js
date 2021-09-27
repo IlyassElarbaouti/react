db976b9059383d176ead134f2ecaf1c5697d4fe0
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-09-27 16:30:46 +0300

    task5

---
+++ lesson1/todo-list/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const root = document.querySelector('#root');
+const element = (
+  <h1 class="title">Todo List</h1>
+)
+ReactDOM.render(
+  element,root
+)
+
