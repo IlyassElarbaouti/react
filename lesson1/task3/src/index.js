3a6db42b6c5a884de5e83ad382a30d4076f65379
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-09-27 14:36:25 +0300

    task3

--- lesson1/task3/src/index.js
+++ lesson1/task3/src/index.js
@@ -10,22 +10,3 @@ const containerElem =(
   </div>
 )
 ReactDOM.render(containerElem,rootElement)
-
-// const renderGreeting = elem => {
-//   const containerElem = document.createElement('div');
-//   containerElem.classList.add('greeting');
-
-//   const titleElem = document.createElement('h1');
-//   titleElem.classList.add('greeting__title');
-//   titleElem.textContent = 'Hello, world!';
-
-//   const textElem = document.createElement('p');
-//   textElem.classList.add('greeting__text');
-//   textElem.textContent = "I'm learning React";
-
-//   containerElem.append(titleElem, textElem);
-
-//   elem.append(containerElem);
-// };
-
-// renderGreeting(rootElement);
