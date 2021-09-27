372dcd733f05f5c3809e1261a302a29de5f15ad1
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-09-27 15:24:30 +0300

    fix

--- lesson1/task4/src/index.js
+++ lesson1/task4/src/index.js
@@ -4,26 +4,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const rootElement = document.querySelector('#root');
-
-// const renderGreeting = elem => {
-//   const containerElem = document.createElement('div');
-//   containerElem.classList.add('greeting');
-
-//   const titleElem = document.createElement('h1');
-//   titleElem.classList.add('greeting__title');
-//   titleElem.textContent = 'Hello, world!';
-
-//   const textElem = document.createElement('p');
-//   textElem.classList.add('greeting__text');
-//   textElem.textContent = "I'm learning React";
-
-//   containerElem.append(titleElem, textElem);
-
-//   elem.append(containerElem);
-// };
-
-// renderGreeting(rootElement);
-
 const element = (
   <div className="greeting">
     <h1 className="greeting__title">Hello, world!</h1>
@@ -32,4 +12,3 @@ const element = (
 );
 
 ReactDOM.render(element, rootElement);
-
