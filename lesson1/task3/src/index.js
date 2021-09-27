9acba172a4e7245c3e9933980823b80c55118be0
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-09-27 14:38:35 +0300

    task1

--- lesson1/task3/src/index.js
+++ lesson1/task3/src/index.js
@@ -3,10 +3,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 const rootElement = document.querySelector('#root');
 
-const containerElem =(
+const element =(
   <div className='greeting'>
     <h1 className='greeting__title'>Hello, world!</h1>
-    <p className='greeting__text'>I'm learning React!</p>
+    <p className='greeting__text'>I'm learning React</p>
   </div>
-)
-ReactDOM.render(containerElem,rootElement)
+);
+ReactDOM.render(element,rootElement)
