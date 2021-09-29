0420994e59e6ff3261c385e72c8e85d26b70ed8f
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-09-29 13:08:45 +0300

    task3

---
+++ lesson2/task3/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+
+const rootElem = document.querySelector('#root');
+const seconds = new Date().getSeconds();
+const bgColor = seconds%2===0 ? 'white' : 'black';
+const color = seconds%2===0 ? 'black' : 'white';
+const styles = {
+    backgroundColor:bgColor,
+    color
+};
+const elem = (
+<div 
+className='seconds'
+style={styles}
+>
+    {seconds}
+</div>
+)
+ReactDOM.render(elem,rootElem)
