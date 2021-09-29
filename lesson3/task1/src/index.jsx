9d50be912bf9cf82d055415336c7d709eaeb3640
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-09-29 14:04:36 +0300

    task1

---
+++ lesson3/task1/src/index.jsx
@@ -0,0 +1,6 @@
+import SearchElem from './Search';
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.scss'
+const rootElem = document.querySelector('#root')
+ReactDOM.render(<SearchElem name='Tom'/>,rootElem)
