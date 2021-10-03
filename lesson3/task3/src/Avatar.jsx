c065531d1f783fccb1db03c8eb7308516346a382
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-10-03 23:45:08 +0300

    task3

--- lesson3/task3/src/Avatar.jsx
+++ lesson3/task3/src/Avatar.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import "./avatar.scss";
-import UserInfo from "./UserInfo";
 
 function Avatar(props) {
   return (
         <img
           className="avatar"
-          src={props.user.avatarUrl}
-          alt={props.user.name}
+          src={props.avatarUrl}
+          alt={props.name}
         />
   );
 }
