f88bab7e8e1beb0907503e56a2899aad485b49d0
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-10-04 12:42:14 +0300

    task2

--- lesson3/task3/src/index.jsx
+++ lesson3/task3/src/index.jsx
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import Comment from './Comment.jsx';
 import './styles.scss'
-const rootElem = document.querySelector('#root')
-ReactDOM.render(<Comment  user={{name:'Ilyass',avatarUrl:'https://avatars.githubusercontent.com/u/63542705?s=96&v=4'}} text='Good job!' date={new Date()}/>,rootElem)
+const rootElem = document.querySelector('#root');
+const userInfo = {
+  name: 'Tom',
+  avatarUrl: 'https://avatars1.githubusercontent.com/users',
+};
+ReactDOM.render(<Comment author={userInfo} text="Good job" date={new Date()}/>,rootElem)
