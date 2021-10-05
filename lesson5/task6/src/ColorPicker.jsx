0fb1739d396f9126b3904770dbfbfb5bbc566037
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-10-05 03:00:09 +0300

    task

--- lesson5/task6/src/ColorPicker.jsx
+++ lesson5/task6/src/ColorPicker.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React , { Component }  from 'react';
 
-class ColorPicker extends React.Component {
+class ColorPicker extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -22,7 +22,7 @@ class ColorPicker extends React.Component {
       <div>
 
         <div className="picker__title">{this.state.title}</div>
-        
+
         <div>
           <button
             className="picker__button picker__button_coral"
