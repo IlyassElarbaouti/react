43fd6096c6ef513f581d3832b259d3038abb00fc
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-10-06 17:45:08 +0300

    task

--- lesson6/task5/src/Auth.jsx
+++ lesson6/task5/src/Auth.jsx
@@ -1,39 +1,38 @@
-import React , { Component }from 'react'
-import Login from './Login.jsx'
-import Spinner from './Spinner.jsx'
-import Logout from './Logout.jsx'
+import React, { Component } from "react";
+import Login from "./Login.jsx";
+import Spinner from "./Spinner.jsx";
+import Logout from "./Logout.jsx";
 
 export default class Auth extends Component {
-    constructor({isLoggedIn}){
-        super({isLoggedIn})
-        this.state={
-            isLoggedIn
-        }
+  constructor({ isLoggedIn }) {
+    super({ isLoggedIn });
+    this.state = {
+      isLoggedIn: false,
+    };
+  }
+  logIn = () => {
+    setTimeout(() => {
+      this.setState({
+        isLoggedIn: "logged",
+      });
+      console.log(this.state.isLoggedIn);
+    }, 2000);
+    this.setState({
+      isLoggedIn: "loading",
+    });
+    console.log(this.state.isLoggedIn);
+  };
+  logOut = () => {
+    this.setState({
+      isLoggedIn: false,
+    });
+  };
+  render() {
+    if (this.state.isLoggedIn === "logged") {
+      return <Logout onLogout={this.logOut} />;
+    } else if (this.state.isLoggedIn === "loading") {
+      return <Spinner />;
     }
-    logIn=()=>{
-       setTimeout(()=>{ this.setState({
-           isLoggedIn:'logged'
-       })
-       console.log(this.state.isLoggedIn)
-       },2000)
-        this.setState({
-           isLoggedIn:'loading'
-       }) 
-        console.log(this.state.isLoggedIn)
-    }
-    logOut=()=>{
-        this.setState({
-           isLoggedIn:false
-       }) 
-    }
-    render() {
-        if(this.state.isLoggedIn==='logged'){
-            return <Logout onLogout={this.logOut}/>
-        }
-        else if(this.state.isLoggedIn==='loading'){
-            return <Spinner />
-        }
-        return <Login onLogin={this.logIn}/>
-    }
+    return <Login onLogin={this.logIn} />;
+  }
 }
-
