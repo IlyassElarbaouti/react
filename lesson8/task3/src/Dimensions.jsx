8f5dfd00cf82a6b6f4a2137e552b18e3a7b1425a
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-10-12 01:13:30 +0300

    task3

--- lesson8/task3/src/Dimensions.jsx
+++ lesson8/task3/src/Dimensions.jsx
@@ -1,37 +1,32 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Dimensions extends Component {
-    state={
-            width:null,
-            height:null,
-        }
+  state = {
+    width: null,
+    height: null,
+  };
 
-    componentDidMount(){
-        window.addEventListener('resize',this.onResize);
-        const {innerWidth,innerHeight}= window;
-        this.setDimensions(innerWidth,innerHeight)
-    }
-    onResize = e =>{
-        const {innerWidth,innerHeight}= e.target;
-        this.setDimensions(innerWidth,innerHeight)
-        
-    }
-    setDimensions=(width,height)=>{
-        this.setState({
-            width,
-            height,
-        })
-       document.title = `${this.state.width} x ${this.state.height}`
-    }
+  componentDidMount() {
+    window.addEventListener("resize", this.onResize);
+    const { innerWidth, innerHeight } = window;
+    this.setDimensions(innerWidth, innerHeight);
+  }
+  onResize = e => {
+    const { innerWidth, innerHeight } = e.target;
+    this.setDimensions(innerWidth, innerHeight);
+  };
+  setDimensions = (width, height) => {
+    this.setState({
+      width,
+      height,
+    });
+    document.title = `${width} x ${height}`;
+  };
 
-    componentWillUnmount(){
-        window.removeEventListener('resize',this.onResize)
-    }
-    render() {
-        return (
-            <div>
-                {`${this.state.width}px - ${this.state.height}px`}
-            </div>
-        )
-    }
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.onResize);
+  }
+  render() {
+    return <div>{`${this.state.width}px - ${this.state.height}px`}</div>;
+  }
 }
