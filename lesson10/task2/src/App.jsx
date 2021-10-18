55856823456478be49fc9a1bebad50f1af3ec34a
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-10-18 18:06:20 +0300

    task2

--- lesson10/task2/src/App.jsx
+++ lesson10/task2/src/App.jsx
@@ -22,7 +22,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="page">
-        <h1 className="title">Hello, {this.state.userName.firstName}</h1>
+        <h1 className="title">Hello, {this.state.userName.firstName} {this.state.userName.lastName}</h1>
         <main className="content">
           <ShoppingCart userName={this.state.userName} />
 
