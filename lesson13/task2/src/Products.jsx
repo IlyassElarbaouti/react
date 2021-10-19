dde285509f973dad4e641f7ebcb9beda66b31c32
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-10-19 17:59:48 +0300

    task2

--- lesson13/task2/src/Products.jsx
+++ lesson13/task2/src/Products.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import Product from './Product.jsx';
-import {Route, Link , Switch} from "react-router-dom";
+import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
+import Product from "./Product.jsx";
 
-const Products = ({match}) => {
-    return (
-      <div className="page__content">
-        <h1>Products</h1>
-        <ul className="navigation">
-          <li className="navigation__item">
-            <Link to={`${match.url}/ball`}>ball</Link>
-          </li>
-          <li className="navigation__item">
-            <Link to={`${match.url}/books`}>books</Link>
-          </li>
-        </ul>
-        <Switch>
-          <Route exact path="/products">
-            <span>Select a product</span>
-          </Route>
-          <Route path={`${match.url}/:productId`} component={Product} />
-        </Switch>
-      </div>
-    );
-}
+const Products = ({ match }) => {
+  return (
+    <div className="page__content">
+      <h1>Products</h1>
+      <ul className="navigation">
+        <li className="navigation__item">
+          <Link to={`${match.url}/book`}>Book</Link>
+        </li>
+        <li className="navigation__item">
+          <Link to={`${match.url}/ball`}>Ball</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route exact path={match.url}>
+          <span>Select a product please</span>
+        </Route>
+        <Route path={`${match.url}/:productId`} component={Product} />
+      </Switch>
+    </div>
+  );
+};
 
-export default Products
+export default Products;
