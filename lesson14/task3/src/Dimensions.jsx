2e4806aed9d313b01f20a7440171f73db941cd34
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-10-20 15:15:54 +0300

    task3

---
+++ lesson14/task3/src/Dimensions.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+
+const Dimensions = () => {
+    const [dimensions,Setdimensions]=useState({
+        width:null,
+        height:null,
+    })
+    useEffect(() => {
+      const { innerWidth, innerHeight } = window;
+      Setdimensions({
+        width: innerWidth,
+        height: innerHeight,
+      });
+      const handleResize=e=>{
+          const {innerHeight,innerWidth} = e.target
+          Setdimensions({
+            width: innerWidth,
+            height: innerHeight,
+          });
+      }
+      window.addEventListener("resize", handleResize);
+      return(
+          window.removeEventListener("resize", handleResize)
+      )
+      
+    }, []);
+
+    const {width,height}=dimensions;
+    return <div class="dimensions">{`${width}px - ${height}px`}</div>;
+}
+
+export default Dimensions
